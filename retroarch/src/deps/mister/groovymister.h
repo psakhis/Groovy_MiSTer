cbcfca72c8c670bb84d980ef2f581f3fe174bc0d
Sergi Clara <sclar000@gmail.com>
2024-04-28 12:49:00 +0200

    retroarch src

---
+++ retroarch/src/deps/mister/groovymister.h
@@ -0,0 +1,163 @@
+#ifndef __GROOVYMISTER_H__
+#define __GROOVYMISTER_H__
+
+#include <inttypes.h>
+
+#ifdef _WIN32
+ #include <winsock2.h>
+ #include <ws2tcpip.h>
+ #include <mswsock.h>
+ #include "rio.h"
+#endif
+
+#ifndef GROOVYMISTER_VERSION
+#define GROOVYMISTER_VERSION "1.0.0"
+#endif
+
+#define BUFFER_SIZE 1245312 // 720x576x3
+#define BUFFER_SLICES 846
+#define BUFFER_MTU 1472
+
+//joystick map
+#define GM_JOY_RIGHT (1 << 0)
+#define GM_JOY_LEFT  (1 << 1)
+#define GM_JOY_DOWN  (1 << 2)
+#define GM_JOY_UP    (1 << 3)
+#define GM_JOY_B1    (1 << 4)
+#define GM_JOY_B2    (1 << 5)
+#define GM_JOY_B3    (1 << 6)
+#define GM_JOY_B4    (1 << 7)
+#define GM_JOY_B5    (1 << 8)
+#define GM_JOY_B6    (1 << 9)
+#define GM_JOY_B7    (1 << 10)
+#define GM_JOY_B8    (1 << 11)
+#define GM_JOY_B9    (1 << 12)
+#define GM_JOY_B10   (1 << 13)
+
+/*! fpgaStatus :
+ *  Data received after CmdInit and CmdBlit calls
+ */
+typedef struct fpgaStatus{
+	uint32_t frame;		//frame on gpu
+	uint32_t frameEcho;	//frame received
+	uint16_t vCount;	//vertical count on gpu
+	uint16_t vCountEcho; 	//vertical received
+
+	uint8_t vramEndFrame; 	//1-fpga has all pixels on vram for last CmdBlit
+	uint8_t vramReady;	//1-fpga has free space on vram
+	uint8_t vramSynced;	//1-fpga has synced (not red screen)
+	uint8_t vgaFrameskip;	//1-fpga used framebuffer (volatile framebuffer off)
+	uint8_t vgaVblank;	//1-fpga is on vblank
+	uint8_t vgaF1;		//1-field for interlaced
+	uint8_t audio;		//1-fgpa has audio activated
+	uint8_t vramQueue; 	//1-fpga has pixels prepared on vram
+} fpgaStatus;
+
+typedef struct fpgaInputs{
+	uint32_t joyFrame;	//joystick blit frame
+	uint8_t  joyOrder;	//joystick blit order
+	uint16_t joy1;	 	//joystick 1 map
+	uint16_t joy2;	 	//joystick 2 map
+} fpgaInputs;
+
+typedef unsigned long DWORD;
+
+class GroovyMister
+{
+ public:
+
+	char *pBufferBlit; 	// This buffer are registered and aligned for sending rgb. Populate it before CmdBlit
+	char *pBufferAudio; 	// This buffer are registered and aligned for sending audio. Populate it before CmdAudio
+	fpgaStatus fpga;	// Data with last received ACK
+	fpgaInputs inputs;      // Data with last inputs received
+
+	GroovyMister();
+	~GroovyMister();
+
+	// Close connection
+	void CmdClose(void);
+	// Init streaming with ip, port, (lz4frames = 0-raw, 1-lz4, 2-lz4hc, 3-lz4 adaptative), soundRate(1-22k, 2-44.1, 3-48 khz), soundChan(1 or 2), rgbMode(0-RGB888, 1-RGBA888, 2-RGB565)
+	uint8_t CmdInit(const char* misterHost, uint16_t misterPort, uint8_t lz4Frames, uint32_t soundRate, uint8_t soundChan, uint8_t rgbMode);
+	// Change resolution (check https://github.com/antonioginer/switchres) with modeline
+	void CmdSwitchres(double pClock, uint16_t hActive, uint16_t hBegin, uint16_t hEnd, uint16_t hTotal, uint16_t vActive, uint16_t vBegin, uint16_t vEnd, uint16_t vTotal, uint8_t interlace);
+	// Stream frame, vCountSync = 0 for auto frame delay or number of vertical line to sync with, margin with nanoseconds for auto frame delay)
+	void CmdBlit(uint32_t frame, uint16_t vCountSync, uint32_t margin);
+	// Stream audio
+	void CmdAudio(uint16_t soundSize);
+	// getACK is used internal on WaitSync, dwMilliseconds = 0 will time out immediately if no new data
+	uint32_t getACK(DWORD dwMilliseconds);
+	// sleep to sync with crt raster
+	void WaitSync(void);
+	// get nanoseconds (positive or negative) to sync with raster
+	int DiffTimeRaster(void);
+
+	void BindInputs(const char* misterHost, uint16_t misterPort);
+	void PollInputs(void);
+
+	void setVerbose(uint8_t sev);
+	const char* getVersion();
+
+ private:
+
+	uint8_t m_verbose;
+
+#ifdef _WIN32
+	SOCKET m_sockFD;
+	RIO_EXTENSION_FUNCTION_TABLE m_rio;
+	RIO_CQ m_sendQueue;
+	RIO_CQ m_receiveQueue;
+	RIO_RQ m_requestQueue;
+	HANDLE m_hIOCP;
+	RIO_BUFFERID m_sendRioBufferId;
+	RIO_BUF m_sendRioBuffer;
+	RIO_BUFFERID m_receiveRioBufferId;
+	RIO_BUF m_receiveRioBuffer;
+	RIO_BUFFERID m_sendRioBufferBlitId;
+	RIO_BUF m_sendRioBufferBlit;
+	RIO_BUF *m_pBufsBlit;
+	RIO_BUFFERID m_sendRioBufferAudioId;
+	RIO_BUF m_sendRioBufferAudio;
+	RIO_BUF *m_pBufsAudio;
+	SOCKET m_sockInputsFD;
+
+	LARGE_INTEGER m_tickStart;
+	LARGE_INTEGER m_tickEnd;
+	LARGE_INTEGER m_tickSync;
+#else
+	int m_sockFD;
+	int m_sockInputsFD;
+
+	struct timespec m_tickStart;
+	struct timespec m_tickEnd;
+	struct timespec m_tickSync;
+#endif
+	struct sockaddr_in m_serverAddr;
+	struct sockaddr_in m_serverAddrInputs;
+	char m_bufferSend[26];
+	char m_bufferReceive[13];
+	char m_bufferInputsReceive[9];
+	char *m_pBufferLZ4;
+	char *m_pBufferAudio;
+	uint8_t m_lz4Frames;
+	uint8_t m_soundChan;
+	uint8_t m_rgbMode;
+	uint32_t m_RGBSize;
+	uint8_t  m_interlace;
+	uint16_t m_vTotal;
+	uint32_t m_frame;
+	uint32_t m_frameTime;
+	uint32_t m_widthTime;
+	uint32_t m_streamTime;
+	uint32_t m_emulationTime;
+
+	char *AllocateBufferSpace(const DWORD bufSize, const DWORD bufCount, DWORD& totalBufferSize, DWORD& totalBufferCount);
+	void Send(void *cmd, int cmdSize);
+	void SendStream(uint8_t whichBuffer, uint32_t bytesToSend, uint32_t cSize);
+	void setTimeStart(void);
+	void setTimeEnd(void);
+	uint32_t DiffTime(void);
+	void setFpgaStatus(void);
+	void setFpgaJoystick(void);
+};
+
+#endif
---
+++ retroarch/src/deps/mister/groovymister_wrapper.cpp
@@ -0,0 +1,241 @@
+/**************************************************************
+
+   groovymister_wrapper.h - GroovyMiSTer C wrapper API header file
+
+   ---------------------------------------------------------
+
+   GroovyMiSTer  noGPU client for Groovy_MiSTer core
+
+ **************************************************************/
+
+#define MODULE_API_EXPORTS_GMW
+#include "groovymister.h"
+#include "groovymister_wrapper.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+GroovyMister* gmw;
+int gmw_inputsBinded;
+
+MODULE_API_GMW void gmw_init(const char* misterHost, uint8_t lz4Frames, uint32_t soundRate, uint8_t soundChan, uint8_t rgbMode)
+{
+	if (gmw == NULL)
+	{
+		gmw = new GroovyMister;
+		gmw_inputsBinded = 0;		
+	}
+	gmw->CmdInit(misterHost, 32100, lz4Frames, soundRate, soundChan, rgbMode);
+}
+
+MODULE_API_GMW void gmw_close(void)
+{
+	if (gmw != NULL)
+	{
+		gmw->CmdClose();
+		delete gmw;
+		gmw = NULL;
+		gmw_inputsBinded = 0;
+	}
+	else
+	{
+		printf("[MiSTer] gmw_close failed\n");
+	}
+}
+
+MODULE_API_GMW void gmw_switchres(double pClock, uint16_t hActive, uint16_t hBegin, uint16_t hEnd, uint16_t hTotal, uint16_t vActive, uint16_t vBegin, uint16_t vEnd, uint16_t vTotal, uint8_t interlace)
+{
+	if (gmw != NULL)
+	{
+		gmw->CmdSwitchres(pClock, hActive, hBegin, hEnd, hTotal, vActive, vBegin, vEnd, vTotal, interlace);
+	}
+	else
+	{
+		printf("[MiSTer] gmw_switchres failed\n");
+	}
+}
+
+MODULE_API_GMW char* gmw_get_pBufferBlit(void)
+{
+	if (gmw != NULL)
+	{
+		return gmw->pBufferBlit;
+	}
+	else
+	{
+		printf("[MiSTer] gmw_get_pBufferBlit failed\n");
+		return NULL;
+	}
+}
+
+MODULE_API_GMW void gmw_blit(uint32_t frame, uint16_t vCountSync, uint32_t margin)
+{
+	if (gmw != NULL)
+	{
+		gmw->CmdBlit(frame, vCountSync, margin);
+	}
+	else
+	{
+		printf("[MiSTer] gmw_blit failed\n");
+	}
+}
+
+MODULE_API_GMW char* gmw_get_pBufferAudio(void)
+{
+	if (gmw != NULL)
+	{
+		return gmw->pBufferAudio;
+	}
+	else
+	{
+		printf("[MiSTer] gmw_get_pBufferAudio failed\n");
+		return NULL;
+	}
+}
+
+MODULE_API_GMW void gmw_audio(uint16_t soundSize)
+{
+	if (gmw != NULL)
+	{
+		gmw->CmdAudio(soundSize);
+	}
+	else
+	{
+		printf("[MiSTer] gmw_audio failed\n");
+	}
+}
+
+MODULE_API_GMW void gmw_waitSync(void)
+{
+	if (gmw != NULL)
+	{
+		gmw->WaitSync();
+	}
+	else
+	{
+		printf("[MiSTer] gmw_waitSync failed\n");
+	}
+}
+
+
+MODULE_API_GMW int gmw_diffTimeRaster(void)
+{
+	if (gmw != NULL)
+	{
+		return gmw->DiffTimeRaster();
+	}
+	else
+	{
+		printf("[MiSTer] gmw_diffTimeRaster failed\n");
+		return 0;
+	}
+}
+
+MODULE_API_GMW uint32_t gmw_getACK(uint8_t dwMilliseconds)
+{
+	if (gmw != NULL)
+	{
+		return gmw->getACK(dwMilliseconds);
+	}
+	else
+	{
+		printf("[MiSTer] gmw_getACK failed\n");
+		return 0;
+	}
+}
+
+MODULE_API_GMW void gmw_getStatus(gmw_fpgaStatus* status)
+{
+	if (gmw != NULL)
+	{
+		status->frame = gmw->fpga.frame;
+		status->frameEcho = gmw->fpga.frameEcho;
+		status->vCount = gmw->fpga.vCount;
+		status->vCountEcho = gmw->fpga.vCountEcho;
+
+		status->vramEndFrame = gmw->fpga.vramEndFrame;
+		status->vramReady = gmw->fpga.vramReady;
+		status->vramSynced = gmw->fpga.vramSynced;
+		status->vgaFrameskip = gmw->fpga.vgaFrameskip;
+		status->vgaVblank = gmw->fpga.vgaVblank;
+		status->vgaF1 = gmw->fpga.vgaF1;
+		status->audio = gmw->fpga.audio;
+		status->vramQueue = gmw->fpga.vramQueue;
+	}
+	else
+	{
+		printf("[MiSTer] gmw_getStatus failed\n");
+	}
+}
+
+MODULE_API_GMW void gmw_bindInputs(const char* misterHost)
+{
+	if (gmw == NULL)
+	{
+		gmw = new GroovyMister();
+		gmw_inputsBinded = 0;
+	}
+	if (!gmw_inputsBinded)
+	{
+		gmw->BindInputs(misterHost, 32101);
+	}	
+	gmw_inputsBinded = 1;
+}
+
+MODULE_API_GMW void gmw_pollInputs(void)
+{
+	if (gmw != NULL)
+	{
+		gmw->PollInputs();
+	}
+	else
+	{
+		printf("[MiSTer] gmw_pollInputs failed\n");
+	}
+}
+
+MODULE_API_GMW void gmw_getInputs(gmw_fpgaInputs* inputs)
+{
+	if (gmw != NULL)
+	{
+		inputs->joyFrame = gmw->inputs.joyFrame;
+		inputs->joyOrder = gmw->inputs.joyOrder;
+		inputs->joy1 = gmw->inputs.joy1;
+		inputs->joy2 = gmw->inputs.joy2;
+	}
+	else
+	{
+		printf("[MiSTer] gmw_getInputs failed\n");
+	}
+}
+
+MODULE_API_GMW const char* gmw_get_version()
+{
+	if (gmw != NULL)
+	{
+		return gmw->getVersion();
+	}
+	else
+	{
+		printf("[MiSTer] gmw_get_version failed\n");
+		return NULL;
+	}
+}
+
+MODULE_API_GMW void gmw_set_log_level(int level)
+{
+	if (gmw != NULL)
+	{
+		gmw->setVerbose(level);
+	}
+	else
+	{
+		printf("[MiSTer] gmw_set_log_level failed\n");
+	}
+}
+
+
+#ifdef __cplusplus
+}
+#endif
